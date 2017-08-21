dbc967261870ea65ee1fb6acdfbe081d66dc3efb
JQ <jq@mjqdeMacBook-Pro.local>
2017-08-21 19:10:29 +0800

    tip

--- 02 Swift _tip/swift3.0 Basic knowledge01.playground/Contents.swift
+++ 02 Swift _tip/swift3.0 Basic knowledge01.playground/Contents.swift
@@ -31,8 +31,62 @@ typealias AudioSample = UInt16
 var maxAudioSample = AudioSample.max
 
 //6.元组 tuples
+let http404Errol = (404,"Not Found")
+let (statusCode,statusMeg) = http404Errol
+statusCode
+statusMeg
+
+http404Errol.0
+http404Errol.1
+
+let http200Status = (statusCode: 200,description: "info")
+http200Status.statusCode
+http200Status.description
 
 
+//7.断言 assertion：来结束代码运行并通过调试来找到值缺失的原因
+//当代码使用优化编译的时候，断言将会被禁用，例如在 Xcode 中，使用默认的 target Release 配置选项来 bu ild 时，断言会被禁用
+let age = 3
+assert(age > 0, "A person's age cannot be less than zero")
+
+//8.三目运算符
+let contentHeight = 40
+let hasHeader = true
+let rowHeight = contentHeight + (hasHeader ? 50 : 20)   //注意空格
+
+//9.空合运算符:空合运算符( a ?? b )将对可选类型 a 进行空判断，如果 a 包含一个值就进行解封，否则就返回一个默认值 b 。表达式 a 必须是 Optional 类型。默认值 b 的类型必须要和 a 存储值的类型保持一致(其实是对： a != nil ? a! : b  的简化)
+
+let defaultColorName = "red"
+var userDefinedColorName: String?
+var colorNameToUse = userDefinedColorName ?? defaultColorName //??注意空格： 如果没有?? 前后没有空格时候，会报错
+
+userDefinedColorName = "green"
+colorNameToUse = userDefinedColorName ?? defaultColorName
+
+
+//10.字符串
+var emptyString = ""
+var anotherEmptyString = String()
+
+if emptyString.isEmpty {
+    print(" 空字符串")
+}
+
+if anotherEmptyString.isEmpty {
+    print(" 空字符串")
+}
+
+//11.字符串是 值类型
+var s6 = "123"
+var s7 = "456"
+s7 = "789"
+
+print(s6)
+print(s7)
+
+for c in "dog !".characters {
+    print(c)
+}
 
 
 
