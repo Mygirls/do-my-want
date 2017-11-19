9562b19532994d5a82c7623a287918a5d1dc69b4
JQ <1208535097@qq.com>
2017-11-19 16:42:24 +0800

    11.20

---
+++ 10 平常练习/Play/Play/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Play
+//
+//  Created by JQ on 2017/11/13.
+//  Copyright © 2017年 Majq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
